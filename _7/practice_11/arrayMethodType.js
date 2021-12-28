98475d40aa397e85c9b2622f3b29bfb67a357706
MahbubTonoy <tnymrbadboy@gmail.com>
2021-12-29 00:39:33 +0600

    clear the workflow

---
+++ _7/practice_11/arrayMethodType.js
@@ -0,0 +1,10 @@
+let name = Array.from("Stack Learner");
+let name1 = "Stack Learner".split("");
+
+ // console.log(name === name1);
+
+ function fuck() {
+  console.log(Array.from(arguments));
+ }
+
+ fuck("Hello World", "Mahbub Rashid Tonoy");
