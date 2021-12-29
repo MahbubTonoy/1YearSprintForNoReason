394636216ff1cee46d33e997218158132a1903ed
MahbubTonoy <tnymrbadboy@gmail.com>
2021-12-30 00:20:49 +0600

    ui project 1

---
+++ _7/practice_12/ui/ui1/res/js/app.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+ $("#pagepiling").pagepiling({
+  verticalCentered: false,
+  // sectionsColor: ['#fff', '#000', '#f0f', '#0f0'],
+  // anchors: ['hi','hi2'],
+  // scrollingSpeed: 300,
+  // easing: 'linear',
+  // loopBottom: true,
+  // loopTop: true,
+  //   navigation: {
+  //    'textColor': 'red',
+  //    'bulletsColor': '#000',
+  //    'position': 'left',
+  //    'tooltips': ['section1', 'section2', 'section3', 'section4']
+  // },
+  // normalScrollElements: ['.innerSection']
+  // touchSensitivity: 5,
+  // animateAnchor: true,
+  //  anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
+  //    menu: '#myMenu'
+  // onLeave: function (index, nextIndex, direction) {
+  //  //after leaving section 2
+  //  if (index == 2 && direction == 'down') {
+  //   alert("Going to section 3!");
+  //  }
+
+  //  else if (index == 2 && direction == 'up') {
+  //   alert("Going to section 1!");
+  //  }
+  // }
+  
+ });
+});
