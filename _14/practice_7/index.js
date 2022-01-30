40f08a97ab874d53a15a4ba484ab7131f90bae1e
MahbubTonoy <tnymrbadboy@gmail.com>
2022-01-31 00:19:47 +0600

    learned request and response method, need more learning curve and practices

--- _14/practice_7/index.js
+++ _14/practice_7/index.js
@@ -1,11 +1,8 @@
 const express = require("express");
-const cookie = require("cookie-parser");
 
 const app = express();
-app.use(cookie());
 
-app.get("*", (req, res) => {
- console.log(req.cookies);
+app.get("/", (req, res) => {
   res.send("Hello World");
 }); 
 
