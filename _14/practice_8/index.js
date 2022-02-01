a531d7ffe2188e2115b819434565db349205b4f4
MahbubTonoy <tnymrbadboy@gmail.com>
2022-02-01 20:12:10 +0600

    clearing the working area

--- _14/practice_8/index.js
+++ _14/practice_8/index.js
@@ -2,9 +2,17 @@ const express = require("express");
 
 const app = express();
 
+const middleWireWithData = (data) => {
+ return (req,res,next) => {
+  res.send(data);
+  next();
+ }
+}
+
+app.use(middleWireWithData("পেয়াজ"));
 
 app.get("/", (req,res)=> {
- res.send("Home Page");
+ console.log("nothing");
 })
 
 app.listen(3000);
