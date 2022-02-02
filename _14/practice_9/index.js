1c983883a62428b7c3e7c8b7bb2fc6fca9668cb9
MahbubTonoy <tnymrbadboy@gmail.com>
2022-02-02 23:49:11 +0600

    learned basics of file uploading

---
+++ _14/practice_9/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const app = express();
+app.set("view engine", "ejs");
+const storage = multer.diskStorage({
+ filename: (err, file, callback) => {
+  let fileFormat = path.extname(file.originalname);
+  let fileName = file.originalname
+                 .replace(" ", "_")
+                 .replace(fileFormat, "") +
+                 "-"+
+                 Date.now() +
+                 fileFormat;
+  callback(null, fileName);
+ },
+ destination: (err, file, callback)=> {
+  callback(null, "./uploads/");
+ }
+});
+
+const uploader = multer({
+ storage,
+})
+
+app.get("/", (req,res)=> {
+ res.render("index");
+});
+
+app.post("/upload", uploader.single("uploadedFile"), (req,res) => {
+ res.send("Files Uploaded!");
+})
+
+app.listen(3000, ()=>{
+ console.log("Server Started at 3000");
+});
