ff93064593e32151ca5c74a5b0a95360bfee8171
MahbubTonoy <tnymrbadboy@gmail.com>
2022-02-02 17:47:08 +0600

    started learning file uploading methodlogy

--- _14/practice_8/index.js
+++ _14/practice_8/index.js
@@ -2,15 +2,6 @@ const express = require("express");
 
 const app = express();
 
-const middleWireWithData = (data) => {
- return (req,res,next) => {
-  res.send(data);
-  next();
- }
-}
-
-app.use(middleWireWithData("পেয়াজ"));
-
 app.get("/", (req,res)=> {
  console.log("nothing");
 })
